ffc3fb9cffb31ec3f275e6748ebfdfae0892ba53
H-OO <38312623+H-OO@users.noreply.github.com>
2018-10-23 13:49:43 +0800

    add content

--- 181004_webpack4+react+typescript/module.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * 帮助 TypeScript 判断传入的参数类型是否正确
- */
-declare let log: (msg: string) => void;
--- 181004_webpack4+react+typescript/src/asset/n.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 命名空间
- * 提供逻辑分组，避免命名冲突问题
- */
-export namespace N {
-  export class A {}
-  export class B {}
-}
